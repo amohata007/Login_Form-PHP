76aaafe8c0db71a742c10af363e8b8ae290d48dc
Abhishek mohata <36244104+amohata007@users.noreply.github.com>
2019-05-14 05:24:10 +0530

    Update login.php

--- login.php
+++ login.php
@@ -5,7 +5,7 @@ session_start();
 
 $con=mysqli_connect("localhost","root","","login");//mysqli("localhost","username of database","password of database","database name")
 $result=mysqli_query($con,"SELECT * FROM `login_info` WHERE `uname`='$uname' && `password`='$password'");
-$count=mysqli_num_rows($result);
+$count=mysqli_num_rows($result);  
 if($count==1)
 {
 	echo "Login success";
